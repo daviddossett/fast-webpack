e134a59b977b7b703af1e389189d29fd16709ed3
mistople <ddossett@microsoft.com>
2021-05-07 15:19:49 -0700

    Init

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import {
+  FASTDesignSystemProvider,
+  FASTCard,
+  FASTButton,
+} from '@microsoft/fast-components';
+
+/*
+ * Ensure that tree-shaking doesn't remove these components from the bundle.
+ */
+FASTDesignSystemProvider;
+FASTCard;
+FASTButton;
